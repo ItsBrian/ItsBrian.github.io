c4d633135b6d7bb8094740a14be385c010709df7
Brian <38297733+ItsBrian@users.noreply.github.com>
2020-04-07 15:11:34 +0200

    fix delays
    If delays were [1000, 2000, 3000] they would be returned as [3000, 2000, 1000].
    Fixed by replacing:
    
    finalOutput = finalOutput + "}, "+delays[0]+");";
    delays.shift();
    
    with
    
    finalOutput = finalOutput + "}, "+delays[delays.length-1]+");";
    delays.pop();

--- dlay.js
+++ dlay.js
@@ -1,22 +1,23 @@
+// dLay Library 
 function dLay(processRaw){
-    finalOutput = "true";
-    delays = [];
-    processClean = processRaw.toString().replace((/^\s+/gm),"");
-    processLines = processClean.split("\n");
-    for (line in processLines){
-        currentLine = processLines[line];
-        currentParts = currentLine.split(" ");
-        if (currentParts[0] == "sleep"){
-            finalOutput = finalOutput + ";setTimeout(function(){";
-            delays.push(currentParts[1]);
-        }
-        else {
-            finalOutput = finalOutput + ";" + currentLine;
-        }
-    }
-    while (delays.length > 0){
-        finalOutput = finalOutput + "}, "+delays[0]+");";
-        delays.shift();
-    }
-    return finalOutput;
+  finalOutput = "true";
+  delays = [];
+  processClean = processRaw.toString().replace((/^\s+/gm),"");
+  processLines = processClean.split("\n");
+  for (line in processLines){
+      currentLine = processLines[line];
+      currentParts = currentLine.split(" ");
+      if (currentParts[0] == "sleep"){
+          finalOutput = finalOutput + ";setTimeout(function(){";
+          delays.push(currentParts[1]);
+      }
+      else {
+          finalOutput = finalOutput + ";" + currentLine;
+      }
+  }
+  while (delays.length > 0){
+      finalOutput = finalOutput + "}, "+delays[delays.length-1]+");";
+      delays.pop();
+  }
+  return finalOutput;
 }
